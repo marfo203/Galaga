71757646fcc3b21d8732fcdd2fd42210d48618c9
Martin Forsberg <marfo203@student.liu.se>
2021-03-03 15:24:44 +0100

    hej

---
+++ src/main/Main.java
@@ -0,0 +1,88 @@
+package main;
+
+import states.GameModel;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+/**
+ * This Class is the entry point of the application.
+ * <p>
+ * This Class has the following primary responsibilities: 1. Serve as the entry
+ * for the Application
+ * <p>
+ * 2. Create the GameModel (For more information about the GameModel see
+ * /src/states/GameModel)
+ * <p>
+ * 3. Create the GameFrame (A HBox Wrapper): (For more information about the
+ * GameFrame see /src/main/GameFrame)...
+ * <p>
+ * 4. Create the GamePanel (A VBOx Wrapper): (For more information about the
+ * GamePanel see /src/main/GamePanel)...
+ * <p>
+ * 5. Run the main loop of the game.
+ * <p>
+ * https://gitlab.liu.se/magni54/ExempelProjekt-TDDE10_725G90
+ * 
+ * @author magni54, alomi60
+ */
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage gameStage) throws Exception {
+		gameStage.setTitle("TotallyAwesomeGame, sort of");
+		gameStage.setWidth(1000);
+		gameStage.setHeight(800);
+		GameModel model = new GameModel();
+		GameFrame frame = new GameFrame(model, 1000, 800);
+		Scene gameScene = new Scene(frame);
+		// Set the target number of frames per second
+		final double targetFps = 50.0;
+		// Calculate frequency in nano seconds
+		final double nanoPerUpdate = 1000000000.0 / targetFps;
+
+		gameStage.setScene(gameScene);
+
+		// We set up a setOnKeyPressed, to handle keyboard input,
+		// like we had a onMouseClick in the canvas for the paint lab.
+		gameScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				// We send it on to the model, to handle it in the various
+				// states of the game.
+				model.keyPressed(event);
+			}
+		});
+
+		// We set an AnimationTimer, to control the flow of the game.
+		new AnimationTimer() {
+			long lastUpdate = 0;
+
+			// This method will be called
+			public void handle(long now) {
+				// Perform game update and game rendering. This will
+				// execute approximately 60 times per second, or as
+				// close to that as possible. Can vary greatly between systems.
+				// If we want closer control we use something like the
+				// if-statement below to control frame rate.
+
+				if ((now - lastUpdate) > nanoPerUpdate) {
+					model.update();
+					frame.repaint();
+					lastUpdate = now;
+				}
+			}
+		}.start(); // We start the timer.
+
+		gameStage.show();
+
+	}
+}
---
+++ src/states/MenuState.java
@@ -0,0 +1,99 @@
+package states;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+import static constants.Constants.SCREEN_HEIGHT;
+import static constants.Constants.SCREEN_WIDTH;
+
+/**
+ * This state represents the menu of the Game The main responsibility of this
+ * class is to allow the user to swap state to the PlayState
+ */
+public class MenuState extends GameState {
+	/*
+	 * The following three variables are just used to show that a change of state
+	 * can be made. The same variables also exist in the PlayState, can you think of
+	 * a way to make this more general and not duplicate variables?
+	 */
+	private String informationText;
+	private Color bgColor;
+	private Color fontColor;
+	// A PlayState, so we can change to the PlayState from the menu.
+	private PlayState playState;
+
+	public MenuState(GameModel model) {
+		super(model);
+		playState = new PlayState(model);
+		informationText = "Press Enter To Play\nEscape to exit";
+		bgColor = Color.GREEN;
+		fontColor = Color.RED;
+	}
+
+	/**
+	 * Draws information text to the screen
+	 */
+	@Override
+	public void draw(GraphicsContext g) {
+		drawBg(g, bgColor);
+
+		g.setFill(fontColor);
+		g.setFont(new Font(30)); // Big letters
+		// Print the information text, centered on the canvas
+		g.fillText(informationText, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+		// Can also use:
+		// g.setStroke(fontColor);
+		// g.strokeText(informationText, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+	}
+
+	/**
+	 *
+	 * @param key KeyEvent representing the pressed key
+	 *
+	 *            This function prints the pressed key to the console it's used to
+	 *            show that a change of state has been made
+	 *
+	 *            For more information see GameState
+	 */
+	@Override
+	public void keyPressed(KeyEvent key) {
+		System.out.println("Trycker på " + key.getText() + " i MenuState");
+
+		if (key.getCode() == KeyCode.ENTER) {
+			model.switchState(playState);
+		} else if (key.getCode() == KeyCode.ESCAPE) {
+			System.exit(0);
+		}
+	}
+
+	/**
+	 * We have nothing to update in the menu, no moving objects etc. so we leave the
+	 * update method empty.
+	 */
+	@Override
+	public void update() {
+	}
+
+	/**
+	 * We currently don't have anything to activate in the MenuState so we leave
+	 * this method empty in this case.
+	 */
+	@Override
+	public void activate() {
+
+	}
+
+	/**
+	 * We currently don't have anything to deactivate in the MenuState so we leave
+	 * this method empty in this case.
+	 */
+
+	@Override
+	public void deactivate() {
+
+	}
+
+}
